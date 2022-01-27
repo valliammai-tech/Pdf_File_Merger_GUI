fbb6c8fda41de361a719d47f323b1c8bf549e7df
valliammai-tech <86144117+valliammai-tech@users.noreply.github.com>
2022-01-27 08:09:08 +0530

    Enhancement
    Added list box to list all pdf files available from user input directory

--- tkinter_python_gui.py
+++ tkinter_python_gui.py
@@ -6,10 +6,15 @@ from pdfFiles import filter_word_file_in_dir
 from pdf_file_merge import merge_pdfs
 
 window=tkinter.Tk()
+files=[]
+listbox=Listbox(window)
 
 def on_click():
     try:
         res=txt.get()
+        files=filter_word_file_in_dir(res)
+        for i in files:
+            listbox.insert('end', i)
         output=merge_pdfs(filter_word_file_in_dir(res))
         if not output:
             messagebox.showerror ("Invalid directory!!", 'Please Enter Valid directory that has PDF files...')
@@ -29,6 +34,7 @@ txt=Entry(window,width=100)
 txt.pack()
 bt=Button(window, text='Search', bg='orange', fg='red', command=on_click)
 bt.pack()
+listbox.pack(fill=X)
 bt1=Button(window, text='Close', bg='blue', fg='yellow', command=window.destroy)
 bt1.pack()
 
